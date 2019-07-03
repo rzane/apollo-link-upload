7391ce266529a47949c7b824257a33347cb2fd80
Ray Zane <raymondzane@gmail.com>
2019-07-02 21:53:41 -0400

    Remove utils module

--- src/extractFiles.ts
+++ src/extractFiles.ts
@@ -1,5 +1,4 @@
-import { map, join, isFileLike } from "./utils";
-import { ReactNativeFile } from './ReactNativeFile';
+import { ReactNativeFile } from "./ReactNativeFile";
 
 export interface ExtractFile {
   path: string;
@@ -11,6 +10,34 @@ export interface ExtractFiles {
   files: ExtractFile[];
 }
 
+const isPlainObject = (value: any) => value && value.constructor === Object;
+
+const isFileLike = (value: any) =>
+  (typeof File !== "undefined" && value instanceof File) ||
+  (typeof Blob !== "undefined" && value instanceof Blob) ||
+  value instanceof ReactNativeFile;
+
+const isFileList = (value: any) =>
+  typeof FileList !== "undefined" && value instanceof FileList;
+
+const join = (left: string, right: string | number) =>
+  left ? `${left}.${right}` : right.toString();
+
+const mapValues = (value: any, fn: (v: any, k: string) => any) =>
+  Object.entries(value).reduce(
+    (acc, [k, v]) => Object.assign(acc, { [k]: fn(v, k) }),
+    {}
+  );
+
+const map = (value: any, fn: (v: any, k: string | number) => any) =>
+  Array.isArray(value)
+    ? value.map(fn)
+    : isPlainObject(value)
+    ? mapValues(value, fn)
+    : isFileList(value)
+    ? Array.from(value).map(fn)
+    : value;
+
 export const extractFiles = (variables: any, path: string = ""): ExtractFiles => {
   if (isFileLike(variables)) {
     return { clone: path, files: [{ path, file: variables }] };
@@ -18,9 +45,9 @@ export const extractFiles = (variables: any, path: string = ""): ExtractFiles =>
 
   const files: ExtractFile[] = [];
   const clone = map(variables, (v, k) => {
-    const result = extractFiles(v, join(path, k));
-    files.push(...result.files);
-    return result.clone;
+    const inner = extractFiles(v, join(path, k));
+    files.push(...inner.files);
+    return inner.clone;
   });
 
   return { clone, files };
